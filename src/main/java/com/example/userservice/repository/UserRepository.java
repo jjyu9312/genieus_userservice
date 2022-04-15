8fb095312343120b4e64ed1a5ad421e3284f0714
kkw <byl9312@gmail.com>
2022-04-15 18:05:02 +0900

    22/04/15 \n MSA 구조 제품 구매 API 만들기 - User Service

--- src/main/java/com/example/userservice/repository/UserRepository.java
+++ src/main/java/com/example/userservice/repository/UserRepository.java
@@ -1,4 +1,9 @@
 package com.example.userservice.repository;
 
-public class UserRepository {
+import com.example.userservice.jpa.UserEntity;
+import org.springframework.data.repository.CrudRepository;
+
+public interface UserRepository extends CrudRepository<UserEntity, Long> {
+
+
 }
--- src/main/java/com/example/userservice/service/UserServiceImpl.java
+++ src/main/java/com/example/userservice/service/UserServiceImpl.java
@@ -2,6 +2,12 @@ package com.example.userservice.service;
 
 
 import com.example.userservice.dto.UserDto;
+import com.example.userservice.jpa.UserEntity;
+import com.example.userservice.repository.UserRepository;
+import org.modelmapper.ModelMapper;
+import org.modelmapper.convention.MatchingStrategies;
+import org.modelmapper.spi.MatchingStrategy;
+import org.springframework.boot.autoconfigure.web.servlet.WebMvcProperties;
 import org.springframework.stereotype.Service;
 
 import java.util.UUID;
@@ -9,9 +15,21 @@ import java.util.UUID;
 @Service
 public class UserServiceImpl implements UserService {
 
+    UserRepository userRepository;
+
+    public UserServiceImpl(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
     @Override
     public UserDto createdUser(UserDto userDto) {
         userDto.setUserId(UUID.randomUUID().toString());
+
+        ModelMapper modelMapper = new ModelMapper();
+        modelMapper.getConfiguration().setMatchingStrategy(MatchingStrategies.STRICT);
+        UserEntity userEntity = modelMapper.map(userDto, UserEntity.class);
+        userRepository.save(userEntity);
+
 
         return null;
     }
